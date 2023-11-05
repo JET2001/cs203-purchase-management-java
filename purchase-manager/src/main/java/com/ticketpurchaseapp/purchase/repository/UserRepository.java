9880f917653f2f444302dc7ee1387b8b4b1cb14c
Jonathan-Teo <teo.junrui.jonathanedward2@gmail.com>
2023-11-06 01:43:38 +0800

    isQueueStillOnGoing, getQueuesForUser and isUserInGroup SQL commands

--- purchase-manager/src/main/java/com/ticketpurchaseapp/purchase/repository/UserRepository.java
+++ purchase-manager/src/main/java/com/ticketpurchaseapp/purchase/repository/UserRepository.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
+import com.ticketpurchaseapp.purchase.dto.Queue;
 import com.ticketpurchaseapp.purchase.dto.User;
 import com.ticketpurchaseapp.purchase.dto.UserAuth;
 import com.ticketpurchaseapp.purchase.dto.UserInfo;
@@ -26,4 +27,13 @@ public interface UserRepository {
     boolean isLoginLocked(@Param("ip_address") String ipAddress, @Param("datetime_recorded") LocalDateTime datetime_recorded);
 
     int removeExpiredLoginRecords(@Param("datetime_recorded") LocalDateTime datetime_recorded);
+
+    boolean hasGroupRegisteredForThisQueue(@Param("group_id") String groupId, @Param("queue_id") String queueId);
+
+    boolean isUserInGroup(@Param("user_id") String userId, @Param("group_id") String groupId);
+    
+    boolean isQueueStillOnGoing(@Param("queue_id") String queueId);
+
+    Queue getQueuesForUser(@Param("user_id") String queueId);
+
 }
